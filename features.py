345fb017ccf3277e22828bf5577767e0d112dab3
Sean Ma <ssma@stanford.edu>
2012-11-26 17:48:44 -0800

    Added isSolvableByAC

--- features.py
+++ features.py
@@ -18,7 +18,6 @@ class Features:
     domains = dict()
     for i in range(0, 9): 
       domains[i] = dict()
-    prev_domain = None
     size_of_domain = 0 
     total_domains = list()
     for round in range(10): 
@@ -32,11 +31,31 @@ class Features:
       size_of_domain = 0
     return total_domains
 
+  def isSolvableByAC(self, sudoku):
+    domains = dict()
+    for i in range(0, 9): 
+      domains[i] = dict()
+    prev_domain = None
+    size_of_domain = 0 
+    while True:
+      for i in range(9): 
+        for j in range(9): 
+          domains[i][j] = sudoku.getLegalMoves(i, j)
+          size_of_domain += len(domains[i][j])
+          if len(domains[i][j]) == 1: 
+            sudoku.setSquare(i, j, domains[i][j][0])
+      if (prev_domain == domains):
+        return False
+      if (sudoku.isComplete):
+        return True
+      prev_domain = domains
+
 
 puzzles = parser.Parser().parse("sudoku_tests.txt")
 feature = Features()
 arc_consistencies = dict()
 for puzzle in puzzles: 
   arc_consistencies[puzzle] = feature.arc_consistency(puzzle)
+  print feature.isSolvableByAC(puzzle)
 for puzzle,values in arc_consistencies.items(): 
   print puzzle.getDifficulty(), values[9] - values[0]
